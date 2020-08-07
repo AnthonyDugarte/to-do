dda30e17631ec9290638145634c2976197aa534b
Anthony Dugarte <toonny1998@gmail.com>
2020-08-07 09:38:56 -0400

    Add simple tasks unit test and validation

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :tasks
+
+  test "Task attributes must not be empty" do
+    task = Task.new
+
+    assert task.invalid?
+
+    assert task.errors[:title].any?
+    assert task.errors[:description].empty?
+  end
+
+  test "Task with just a tittle should be a valid one" do
+    task = Task.new(title: tasks(:one).title)
+
+    assert task.valid?
+  end
 end
