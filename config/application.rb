247733537367127dcbec8a519adf2e58dde4f4c2
Anthony Dugarte <toonny1998@gmail.com>
2020-08-07 07:40:06 -0400

    Add tailwind setup

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module ChatRailsApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,30 @@
 module.exports = {
   plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: 'no-2009',
       },
-      stage: 3
-    })
-  ]
+      stage: 3,
+    }),
+  ],
+};
+
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  );
 }
